40a8ed8801f0a5e236385cda546e803648bbd220
thaminyteixeira <thaminynha_teixeira@hotmail.com>
2015-03-16 10:14:25 -0300

    Create ProgramaJava1.java
    Criando primeiro programa Java no terminal

---
+++ aulas/ProgramaJava1.java
@@ -0,0 +1,6 @@
+public class ProgramaJava1{
+	public static void main(String [] args) {
+		/* Imprime no console a frase: 'Meu primeiro programa em Java' */
+		System.out.println ("Meu primeiro programa Java");
+	}
+}
