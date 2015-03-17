582e1f266a3d73f17f9a51a7e8c560d58badd670
thaminyteixeira <thaminynha_teixeira@hotmail.com>
2015-03-17 10:53:25 -0300

    Create Exercicio1_5.java
    Resolução do exercício 5

---
+++ exercicio1/Exercicio1_5.java
@@ -0,0 +1,12 @@
+public class Exercicio1_5{
+	public static void main (String [] args){
+		double a=45;
+		double b=20;
+		double c=3;
+		System.out.println ("O valor da soma foi: " + (a+b+c));
+		System.out.println ("O valor da multiplicacao foi: " + (a*b*c));
+		System.out.println ("O valor da divisao foi: " + (a/b/c));
+		System.out.println ("O valor da subtracao foi: " + (a-b-c));
+
+	}
+}
