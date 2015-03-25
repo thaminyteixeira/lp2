ef47f73cdc9f007a0bc72df12cc5b2d4d0a3fa9f
thaminyteixeira <thaminynha_teixeira@hotmail.com>
2015-03-25 16:30:51 -0300

    Create Exponencial.java

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,12 @@
+public class Exponencial{
+	
+
+	public static void main (String [] args){
+		System.out.println ("#        Q        C");		
+		for (int i=0; i<=10; i++){
+			int quadrado=i*i;
+			int cubo=i*i*i;
+			System.out.printf ("%d\t %d\t %d\n", i, quadrado, cubo); 	
+		}
+	}
+}
